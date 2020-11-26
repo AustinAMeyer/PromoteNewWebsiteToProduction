4ad4a491602d00fd4ed9cac3c1948d7f0bc54744
Austin Meyer <austinmeyer@austins-mbp.lan>
2020-11-26 17:33:26 -0600

    Initial commit premote to production

---
+++ executeTemplate.sh
@@ -0,0 +1,2 @@
+S3_BUCKET_NAME=lesson4section28bucket
+aws cloudformation deploy --template-file cloudfront.yml --stack-name production-distro --parameter-overrides PipelineID="${S3_BUCKET_NAME}" --tags project=udapeople
